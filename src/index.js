3922a94a6b832e1258c3db5c4a962bce469af6be
Haileab <womahaileab1428@gmail.com>
2025-11-06 00:58:00 +0300

    fixed the gird

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./style.css";
+const grids = document.getElementsByClassName('board');
+const rows = document.getElementsByClassName("gridrow");
+for (let g of grids) {  // loop over both boards
+  for (let i = 0; i < 10; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('gridrow');
+    g.appendChild(cell); // append to the current board
+  }
+}
+for (let r of rows){
+    for ( let i = 0; i < 10; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('gridcol');
+        r.appendChild(cell);
+    }
+}
