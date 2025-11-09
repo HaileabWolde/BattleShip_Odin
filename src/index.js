52c92ef4f91443f54db79c4e6c47706440835d20
Haileab <womahaileab1428@gmail.com>
2025-11-09 22:56:18 +0300

    started working on how to recivie the coordinates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import "./style.css";
 const grids = document.getElementsByClassName('board');
 const rows = document.getElementsByClassName("gridrow");
+
 for (let g of grids) {  // loop over both boards
   for (let i = 0; i < 10; i++) {
     const cell = document.createElement('div');
     cell.classList.add('gridrow');
+    cell.id = `${i}`
     g.appendChild(cell); // append to the current board
   }
 }
@@ -12,6 +14,13 @@ for (let r of rows){
     for ( let i = 0; i < 10; i++){
         const cell = document.createElement('div');
         cell.classList.add('gridcol');
+        cell.id = `${i}`
         r.appendChild(cell);
     }
+}
+const sendCoordinates = (rowId,e)=>{
+  console.log(`${rowId}${e.target.id}`)
+}
+for (let r of rows){
+ r.addEventListener('click', (e) => sendCoordinates(r.id, e))
 }
