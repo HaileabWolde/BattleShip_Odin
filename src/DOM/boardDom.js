48e550b5ff5895574e35715d7a163f371718a127
Haileab <womahaileab1428@gmail.com>
2025-11-18 23:10:51 +0300

    code refactoring

--- src/DOM/boardDom.js
+++ src/DOM/boardDom.js
@@ -2,85 +2,66 @@ import Player from "../game/Player";
 import ship from "../game/ship";
 import shipColors from "./shipColor";
 import cacheDom from "./cacheDom";
+
 const playerOne = new Player("JackSparrow");
 const playerTwo = new Player("DavyJones");
-export const boardDom = (e, r, parentNode)=>{
-  let { shipTypeOne,shipTypeTwo,  gridOne, gridTwo} = cacheDom();
-      let grid = (parentNode === "boardOne") ? gridOne : gridTwo;
-      let shipType = (parentNode === "boardOne") ? shipTypeOne : shipTypeTwo
-      const shipOne = new ship(shipType);
-      const row = parseInt(r.id);
-      const col = parseInt(e.target.id);
-      for ( let i = 0; i < shipType; i++){
-        if(grid === 'horizontal'){
-          if((row >= 10) || (col + i >= 10)){
-            alert(`You Can't place ${shipOne.name} ship here`)
-            return false;
-          }
-          else  if(parentNode === "boardOne") {
-              if(playerOne.checkCoordinate(row, col + i)){
-                alert('The Cell is Occupied By Another Ship')
-                 return false;
-              }
-          }
-          else  {
-               if(playerTwo.checkCoordinate(row, col + i)){
-               alert('The Cell is Occupied By Another Ship')
-                 return false;
-             }
-          }
-        }
-        else {
-          if((row + i >= 10) || (col >= 10)){
-            alert(`You Can't place ${shipOne.name} ship here`)
-            return false;
-          }
-           else  if(parentNode === "boardOne") {
-              if(playerOne.checkCoordinate(row + i, col)){
-                alert('The Cell is Occupied By Another Ship')
-                 return false;
-              }
-          }
-          else {
-              if(playerTwo.checkCoordinate(row + i, col)){
-                  alert('The Cell is Occupied By Another Ship')
-                  return false;
-              }
-          }
-        }
-      }
-     
-      for (let i = 0; i < shipType; i++) {
-             if (grid === 'horizontal') {
-                  if(parentNode === "boardOne"){
-                    playerOne.placeCoordinate(shipOne, row, col + i);
-                  }
-                   else{
-                    playerTwo.placeCoordinate(shipOne, row, col + i);
-                   }
-                  
-               const cellToColor = document.querySelector(
-                        `.board[id='${parentNode}'] .gridrow[id='${row}'] .gridcol[id='${col + i}']`
-                  );
-                if(cellToColor){
-                  cellToColor.style.backgroundColor = shipColors[shipOne.name];
-                }
-              
-        } 
-           else {
-                  if(parentNode === "boardOne" ){
-                      playerOne.placeCoordinate(shipOne, row + i, col);
-                  }
-                  else {
-                      playerTwo.placeCoordinate(shipOne, row + i, col);
-                  }
-                  const cellToColor = document.querySelector(
-                      `.board[id='${parentNode}'] .gridrow[id='${row + i}'] .gridcol[id='${col}']`
-                );
-                if(cellToColor){
-                  cellToColor.style.backgroundColor = shipColors[shipOne.name];
-                }
-    }}
 
-      return true;
-}
+export const boardDom = (e, rowElement, boardId) => {
+  const { shipTypeOne, shipTypeTwo, gridOne, gridTwo } = cacheDom();
+
+  // Which board is being used?
+  const isBoardOne = boardId === "boardOne";
+  const currentGridDirection = isBoardOne ? gridOne : gridTwo;
+  const shipType = isBoardOne ? shipTypeOne : shipTypeTwo;
+  const player = isBoardOne ? playerOne : playerTwo;
+
+  const shipInstance = new ship(shipType);
+
+  const row = parseInt(rowElement.id);
+  const col = parseInt(e.target.id);
+
+  // --------------------------
+  // Helpers
+  // --------------------------
+
+  const outOfBounds = (r, c) => r >= 10 || c >= 10;
+
+  const cellOccupied = (r, c) => player.checkCoordinate(r, c);
+
+  const colorCell = (r, c) => {
+    const selector = `.board[id='${boardId}'] .gridrow[id='${r}'] .gridcol[id='${c}']`;
+    const cell = document.querySelector(selector);
+    if (cell) cell.style.backgroundColor = shipColors[shipInstance.name];
+  };
+
+  // --------------------------
+  // Validation Loop
+  // --------------------------
+  for (let i = 0; i < shipType; i++) {
+    const targetRow = currentGridDirection === "horizontal" ? row : row + i;
+    const targetCol = currentGridDirection === "horizontal" ? col + i : col;
+
+    if (outOfBounds(targetRow, targetCol)) {
+      alert(`You can't place ${shipInstance.name} ship here.`);
+      return false;
+    }
+
+    if (cellOccupied(targetRow, targetCol)) {
+      alert("This cell is already occupied by another ship.");
+      return false;
+    }
+  }
+
+  // --------------------------
+  // Place Ship (after validated)
+  // --------------------------
+  for (let i = 0; i < shipType; i++) {
+    const targetRow = currentGridDirection === "horizontal" ? row : row + i;
+    const targetCol = currentGridDirection === "horizontal" ? col + i : col;
+
+    player.placeCoordinate(shipInstance, targetRow, targetCol);
+    colorCell(targetRow, targetCol);
+  }
+
+  return true;
+};
